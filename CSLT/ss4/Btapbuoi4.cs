054fbc72e9e1412dd2248d9176928394076ad189
nguyengiabaotuong <nguyengiabaotuong@gmail.com>
2026-08-24 16:24:19 +0700

    Btap buoi 4

--- CSLT/ss4/Btapbuoi4.cs
+++ CSLT/ss4/Btapbuoi4.cs
@@ -91,7 +91,55 @@ internal class Btapbuoi4
                     Console.WriteLine("It's another symbol.");
                 }
             }
-        bai5();
+        static void bai6()
+        {
+            Console.Write("Nhap he so a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap he so b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap he so c: ");
+            double c = double.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    if (c == 0)
+                    {
+                        Console.WriteLine("Phuong trinh vo nghiem");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Phuong trinh vo so nghiem");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Day la phuong trinh bac 1. Nghiem la: x = {-c / b:F2}");
+                }
+                return; 
+            }
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+            if (delta < 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem.");
+            }
+            else if (delta == 0)
+            {
+                double kep = -b / (2 * a);
+                Console.WriteLine($"Phuong trinh co nghiem kep: x1 = x2 = {kep:F2}");
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Phuong trinh co 2 nghiem phan biet:");
+                Console.WriteLine($"x1 = {x1:F2}");
+                Console.WriteLine($"x2 = {x2:F2}");
+            }
+        }
+        bai6();
     }
 }
 
